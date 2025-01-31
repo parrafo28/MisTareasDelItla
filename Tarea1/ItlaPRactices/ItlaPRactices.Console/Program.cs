fdd98e413000a5bdb6acc75ca3552df927eadf95
Starling Germosen <sgermosen@outlook.com>
2025-01-31 18:47:44 -0400

    Agregando mi tarea uno

---
+++ Tarea1/ItlaPRactices/ItlaPRactices.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+asyncdas
+    da
+    sd
+    asd
+    a
+    sd
+    a
+    sd
+    as
+    decimalas
+    d
